c6fd02eeb4406093507fa6d559dff0aa1f9be382
B3Z0 <vlad.cotiga75@gmail.com>
2025-07-31 16:33:43 +0200

    initial project setup

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+mod error;
+mod tiled;
+mod layer;
+
+pub use error::Error;
+pub use tiled::Map;
+pub use layer::Layer;
+
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
